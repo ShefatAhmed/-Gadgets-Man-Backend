5267e81d7a51c3c1a0fb2f743b71f11cde87a46e
unknown <shefata69@gmail.com>
2024-03-22 00:58:42 +0600

    flash sale

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,33 +12,59 @@ app.use(express.json());
 
 // MongoDB Connection URL
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-        // Connect to MongoDB
-        await client.connect();
-        console.log("Connected to MongoDB");
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
 
-        const db = client.db('assignment');
-        const collection = db.collection('users');
+    const db = client.db("gadget");
+    const flashSalecollection = db.collection("flashSale");
 
-        // Start the server
-        app.listen(port, () => {
-            console.log(`Server is running on http://localhost:${port}`);
-        });
+    // flash
+    app.post("/api/v1/flash-sale", async (req, res) => {
+      const { name, image, amount, offer, flashSale } = req.body;
+      const createdAt = new Date();
+      const defaultFlashSale = true;
+      const data = await flashSalecollection.insertOne({
+        name,
+        image,
+        amount,
+        offer,
+        createdAt,
+        flashSale: flashSale !== undefined ? flashSale : defaultFlashSale,
+      });
+      res.send(data);
+    });
 
-    } finally {
-    }
+    app.get("/api/v1/flash-sale", async (req, res) => {
+      const result = await flashSalecollection
+        .find({ flashSale: true })
+        .sort({ createdAt: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
 // Test route
-app.get('/', (req, res) => {
-    const serverStatus = {
-        message: 'Server is running smoothly',
-        timestamp: new Date()
-    };
-    res.json(serverStatus);
+app.get("/", (req, res) => {
+  const serverStatus = {
+    message: "Server is running smoothly",
+    timestamp: new Date(),
+  };
+  res.json(serverStatus);
 });
